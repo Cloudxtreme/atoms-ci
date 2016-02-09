4cf4c398399f163cb4fb3311766a1033c93a7ee5
atomsd <support@atomsd.org>
2016-02-09 11:44:08 +0000

    Add debian/ubuntu to runtime tests

--- setup-runtime-nodes.sh
+++ setup-runtime-nodes.sh
@@ -41,20 +41,20 @@ ${SCRIPT_PATH}/setup.sh --name="atomsd/jenkins-runtime:centos7" --file="${SCRIPT
 #${SCRIPT_PATH}/setup.sh --name="atomsd/jenkins-runtime:fedora22" --file="${SCRIPT_PATH}/fedora22/runtime" --host=fedora22-runtime ${REBUILD_STR} --privileged
 ${SCRIPT_PATH}/setup.sh --name="atomsd/jenkins-runtime:ubuntu14" --file="${SCRIPT_PATH}/ubuntu14/runtime" --host=ubuntu14-runtime ${REBUILD_STR} --privileged
 ${SCRIPT_PATH}/setup.sh --name="atomsd/jenkins-runtime:ubuntu15" --file="${SCRIPT_PATH}/ubuntu15/runtime" --host=ubuntu15-runtime ${REBUILD_STR} --privileged
-#${SCRIPT_PATH}/setup.sh --name="atomsd/jenkins-runtime:debian8" --file="${SCRIPT_PATH}/debian8/runtime" --host=debian8-runtime ${REBUILD_STR} --privileged
+${SCRIPT_PATH}/setup.sh --name="atomsd/jenkins-runtime:debian8" --file="${SCRIPT_PATH}/debian8/runtime" --host=debian8-runtime ${REBUILD_STR} --privileged
 
 cp nginx-docker.tmpl /etc/nginx/conf.d/docker-centos6-runtime.conf
 cp nginx-docker.tmpl /etc/nginx/conf.d/docker-centos7-runtime.conf
 #cp nginx-docker.tmpl /etc/nginx/conf.d/docker-fedora22-runtime.conf
 cp nginx-docker.tmpl /etc/nginx/conf.d/docker-ubuntu14-runtime.conf
 cp nginx-docker.tmpl /etc/nginx/conf.d/docker-ubuntu15-runtime.conf
-#cp nginx-docker.tmpl /etc/nginx/conf.d/docker-debian8-runtime.conf
+cp nginx-docker.tmpl /etc/nginx/conf.d/docker-debian8-runtime.conf
 
 sed -i -e s/SUB_DOMAIN/centos6-runtime/g /etc/nginx/conf.d/docker-centos6-runtime.conf
 sed -i -e s/SUB_DOMAIN/centos7-runtime/g /etc/nginx/conf.d/docker-centos7-runtime.conf
 #sed -i -e s/SUB_DOMAIN/fedora22-runtime/g /etc/nginx/conf.d/docker-fedora22-runtime.conf
 sed -i -e s/SUB_DOMAIN/ubuntu14-runtime/g /etc/nginx/conf.d/docker-ubuntu14-runtime.conf
 sed -i -e s/SUB_DOMAIN/ubuntu15-runtime/g /etc/nginx/conf.d/docker-ubuntu15-runtime.conf
-#sed -i -e s/SUB_DOMAIN/debian8-runtime/g /etc/nginx/conf.d/docker-debian8-runtime.conf
+sed -i -e s/SUB_DOMAIN/debian8-runtime/g /etc/nginx/conf.d/docker-debian8-runtime.conf
 
 systemctl restart nginx
